e9b452176ff568898a3e9d485e33ea670ca2f010
Andre Lima <andresdslima@gmail.com>
2022-04-23 13:53:01 +0200

    fixing delete button

--- Components/deleteTask.js
+++ Components/deleteTask.js
@@ -1,5 +1,6 @@
 const DeleteButton = () => {
     const buttonToDelete = document.createElement('button');
+    buttonToDelete.classList.add('delete-button');
     buttonToDelete.innerText = 'Delete';
     buttonToDelete.addEventListener('click', deleteTask);
     return buttonToDelete;
